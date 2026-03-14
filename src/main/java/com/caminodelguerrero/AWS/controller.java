82f9847543caedca09b53b0e4fa2e9da812118f0
Reyin <ReyinLight@gmail.com>
2026-03-13 21:18:25 -0600

    Primer commit perras

---
+++ src/main/java/com/caminodelguerrero/AWS/controller.java
@@ -0,0 +1,13 @@
+package com.caminodelguerrero.AWS;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping
+    public String saludoCordial(){
+        return "<H1>Saludos cordiales perras! :D</H1>";
+    }
+}
